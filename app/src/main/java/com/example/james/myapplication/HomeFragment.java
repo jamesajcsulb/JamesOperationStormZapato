232b337a30903d2581df9ddd5f3faaa6954e599d
jamesajcsulb <anjin.gang@student.csulb.edu>
2018-04-30 17:15:10 -0700

    Recycler view initiated. Home browse product screen scrolls clean

--- app/src/main/java/com/example/james/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/james/myapplication/HomeFragment.java
@@ -25,6 +25,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestListener;
@@ -136,9 +137,15 @@ public class HomeFragment extends Fragment
                 //Initialize ImageView
 
                 MyAdapter myadapter=new MyAdapter(getActivity(),shoearraypass);
-                mListView.setAdapter(myadapter);
+                //mListView.setAdapter(myadapter);
 
 
+
+
+                /////
+                //List<String> list = ..;
+                String[] arrayshoeconv = shoearraypass.toArray(new String[0]);
+                /////
 
                 //setContentView(R.layout.activity_main);
 
@@ -146,10 +153,15 @@ public class HomeFragment extends Fragment
                 String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
 
                 // set up the RecyclerView
+                //RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.rvNumbers);
+                //int numberOfColumns = 6;
+                //recyclerView.setLayoutManager(new GridLayoutManager(getContext(), numberOfColumns));
+                //adapter = new MyRecyclerViewAdapter(getContext(), data);
+
                 RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.rvNumbers);
-                int numberOfColumns = 6;
+                int numberOfColumns = 3;
                 recyclerView.setLayoutManager(new GridLayoutManager(getContext(), numberOfColumns));
-                adapter = new MyRecyclerViewAdapter(getContext(), data);
+                adapter = new MyRecyclerViewAdapter(getContext(), arrayshoeconv);
                 //adapter.setClickListener(this);
                 recyclerView.setAdapter(adapter);
             }
