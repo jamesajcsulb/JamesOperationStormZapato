6e1867cb5cfd1e56993140e6f1bdf81851481fc0
jamesajcsulb <anjin.gang@student.csulb.edu>
2018-05-03 21:36:19 -0700

    Search user initialize

--- app/src/main/java/com/example/james/myapplication/search/SearchFragment.java
+++ app/src/main/java/com/example/james/myapplication/search/SearchFragment.java
@@ -1,13 +1,22 @@
 package com.example.james.myapplication.search;
 
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.james.myapplication.R;
+import com.google.firebase.database.Query;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SearchFragment extends Fragment
 {
@@ -23,12 +32,25 @@ public class SearchFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-
-        //container.removeAllViews();
         View v = inflater.inflate(R.layout.fragment_search, container, false);
-        if (v != null)
-        {
-        }
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
+        myRef.child("21fGOBPcK8ZffFD1NV3OgV8ofLk1").child("").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    Toast.makeText(getContext(),"Agustin Barajas" + dataSnapshot.getKey(),Toast.LENGTH_LONG).show();
+                } else {
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getContext(),"doesn't exist you are fucked xD",Toast.LENGTH_LONG).show();
+
+            }
+        });
 
         ListView mListView = (ListView) getActivity().findViewById(R.id.staticListView);
         mListView.setAdapter(null);
