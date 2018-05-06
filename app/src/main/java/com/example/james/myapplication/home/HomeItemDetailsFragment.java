64e1d4d1f4ae948a50b4493418036ec459b6790f
jamesajcsulb <anjin.gang@student.csulb.edu>
2018-05-06 11:17:41 -0700

    Purchase firebase link initialized

--- app/src/main/java/com/example/james/myapplication/home/HomeItemDetailsFragment.java
+++ app/src/main/java/com/example/james/myapplication/home/HomeItemDetailsFragment.java
@@ -4,23 +4,16 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.james.myapplication.models.MyRecyclerViewAdapterShoes;
-import com.example.james.myapplication.PurchaseStepOneFragment;
 import com.example.james.myapplication.R;
-import com.example.james.myapplication.profile.ProfileFragment;
 
 public class HomeItemDetailsFragment extends Fragment
 {
