db05d7274728eaeb76756aded8942ebd62aa2482
jamesajcsulb <anjin.gang@student.csulb.edu>
2018-05-06 00:04:47 -0700

    Search page pulling data from firebase

---
+++ app/src/main/java/com/example/james/myapplication/search/SearchRecyclerViewAdapter.java
@@ -0,0 +1,137 @@
+package com.example.james.myapplication.search;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.james.myapplication.MainActivity;
+import com.example.james.myapplication.PurchaseHistory.PurchaseHistoryFragmentDetails;
+import com.example.james.myapplication.R;
+import com.example.james.myapplication.home.HomeItemDetailsFragment;
+
+import java.util.ArrayList;
+
+public class SearchRecyclerViewAdapter extends RecyclerView.Adapter<SearchRecyclerViewAdapter.ViewHolder> {
+
+    private String[] mData = new String[0];
+    private ArrayList<String> mShoe = new ArrayList<String>();
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+    private Context context;
+    private ImageView myImageView;
+    private TextView myTextView;
+    private TextView myBrandTextView;
+    public ImageView recyclerimageView;
+    private Activity ac;
+    private Fragment fra;
+    private FragmentManager frasss;
+
+    // data is passed into the constructor
+    public SearchRecyclerViewAdapter(Context context, ArrayList<String> shoeshoe, Context con, Activity a, Fragment fragm) {
+        this.mInflater = LayoutInflater.from(context);
+        //this.mData = data;
+        this.context = con;
+        this.ac = a;
+        this.fra = fragm;
+        this.mShoe = shoeshoe;
+    }
+
+    // inflates the cell layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.search_recyclerview_item, parent, false);
+
+        //recyclerimageView=(ImageView)view.findViewById(R.id.round_price);
+        //myImageView=(ImageView)view.findViewById(R.id.recyclerlistitemimageview);
+        //myTextView=(TextView)view.findViewById(R.id.textView2);
+        myBrandTextView=(TextView)view.findViewById(R.id.brand);
+
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the textview in each cell
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ArrayList<String> arrayList = mShoe;
+        //Glide.with(myImageView.getContext())
+        //        .load(""+arrayList.get(position).getShoeImageUrl())
+        //        .into(myImageView);
+        //myTextView.setText("" + arrayList.get(position).getShoePrice());
+        myBrandTextView.setText("" + arrayList.get(position).toString());
+    }
+
+    // total number of cells
+    @Override
+    public int getItemCount() {
+        return mShoe.size();
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            FragmentManager fragmentManagerssss = ((MainActivity)context).getSupportFragmentManager();
+
+            Fragment fragment = new PurchaseHistoryFragmentDetails();
+            Bundle arguments = new Bundle();
+            arguments.putString( "itemId" , "" + getAdapterPosition());
+            //arguments.putString( "itemPicture" , "" + mShoe.get(getAdapterPosition()).getShoeImageUrl());
+            //arguments.putString( "itemDescription" , "" + mShoe.get(getAdapterPosition()).getShoePrice()
+            //        + " " + mShoe.get(getAdapterPosition()).getShoeBrand());
+            fragment.setArguments(arguments);
+            final FragmentTransaction ft = fragmentManagerssss.beginTransaction();
+            ft.replace(R.id.fragment_container, fragment);
+            ft.commit();
+
+            if (mClickListener != null) {
+                mClickListener.onItemClick(view, getAdapterPosition());
+            }
+        }
+    }
+
+    // convenience method for getting data at click position
+    String getItem(int id) {
+        return mData[id];
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    private void fragmentJump(int i) {
+        fra = new HomeItemDetailsFragment();
+        switchContent(R.id.fragment_container, fra);
+    }
+
+    public void switchContent(int id, Fragment fragment) {
+        if (context == null)
+            return;
+        if (context instanceof MainActivity) {
+            MainActivity mainActivity = (MainActivity) context;
+            Fragment frag = fragment;
+            mainActivity.switchContent(id, frag);
+        }
+    }
+}
--- app/src/main/java/com/example/james/myapplication/search/fragment_speech.java
+++ app/src/main/java/com/example/james/myapplication/search/fragment_speech.java
@@ -6,14 +6,27 @@ import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.james.myapplication.PurchaseHistory.PurchaseHistoryRecyclerViewAdapter;
 import com.example.james.myapplication.R;
+import com.example.james.myapplication.home.HomeFragment;
+import com.example.james.myapplication.models.ImageAdapter;
+import com.example.james.myapplication.models.Shoe;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -25,6 +38,16 @@ public class fragment_speech extends Fragment {
     private TextView voiceInput;
     private TextView speakButton;
     private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    private GridView gridView = null;
+    private ArrayList<Shoe> mShoeList = null;
+    private ImageAdapter mImageAdapter = null;
+    private ArrayList<String> shoearraypass = null;
+    private ArrayList<Shoe> classshoe = null;
+    private ArrayAdapter<Shoe> adap = null;
+    private ImageAdapter imageViewing = null;
+    private RecyclerView recycled = null;
+    private PurchaseHistoryRecyclerViewAdapter adaptershoe;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -41,6 +64,38 @@ public class fragment_speech extends Fragment {
             @Override
             public void onClick(View v) {
                 askSpeechInput();
+            }
+        });
+
+
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("search");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                shoearraypass = new ArrayList<String>();
+                //classshoe = new ArrayList<Shoe>();
+                //String ds = dataSnapshot.child("DrmYuz6ApObAgBKGjlQM2JvER8t1").getKey();
+                for (DataSnapshot snaparray : dataSnapshot.getChildren()) {
+                    //for(DataSnapshot s2 : snaparray.child("6buy_history").getChildren()) {
+                    //shoearraypass.add("" + s2.getKey());
+                    //classshoe.add("");
+                    shoearraypass.add(snaparray.getValue().toString());
+                    //Toast.makeText(getContext(),"Peta: " + snaparray.getValue(), Toast.LENGTH_SHORT).show();
+                    //}
+                }
+
+                RecyclerView recyclerView2 = (RecyclerView) getActivity().findViewById(R.id.recycler_view);
+                int numberOfColumns2 = 1;
+                recyclerView2.setLayoutManager(new GridLayoutManager(getContext(), numberOfColumns2));
+                adaptershoe = new PurchaseHistoryRecyclerViewAdapter(getContext(), shoearraypass, getContext(), getActivity(), new HomeFragment() );
+                recyclerView2.setAdapter(adaptershoe);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
             }
         });
 
