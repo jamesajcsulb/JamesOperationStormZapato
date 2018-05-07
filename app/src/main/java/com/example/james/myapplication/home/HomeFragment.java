daeae16c1e431c10cad8efa41ae6d4e621af3fe0
jamesajcsulb <anjin.gang@student.csulb.edu>
2018-05-06 21:54:14 -0700

    Check push

--- app/src/main/java/com/example/james/myapplication/home/HomeFragment.java
+++ app/src/main/java/com/example/james/myapplication/home/HomeFragment.java
@@ -76,7 +76,7 @@ public class HomeFragment extends Fragment
 
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("users");
+        DatabaseReference myRef = database.getReference("shoes");
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -84,9 +84,9 @@ public class HomeFragment extends Fragment
                 shoearraypass = new ArrayList<String>();
                 classshoe = new ArrayList<Shoe>();
                 //OS8hpHlgYRgTB73CAC4EC7badD82
-                //String ds = dataSnapshot.child("OS8hpHlgYRgTB73CAC4EC7badD82").getKey();
-                //for (DataSnapshot snaparray : dataSnapshot.child("OS8hpHlgYRgTB73CAC4EC7badD82").getChildren()) {
-                for (DataSnapshot snaparray : dataSnapshot.child("OS8hpHlgYRgTB73CAC4EC7badD82").child("9sellinventory").child("shoes").child("b22mhFiqHuVR9vwowQyQjD4720Q2").getChildren()) {
+                //String ds = dataSnapshot.child("b22mhFiqHuVR9vwowQyQjD4720Q2").getKey();
+                for (DataSnapshot snaparray : dataSnapshot.child("b22mhFiqHuVR9vwowQyQjD4720Q2").getChildren()) {
+                //for (DataSnapshot snaparray : dataSnapshot.child("OS8hpHlgYRgTB73CAC4EC7badD82").child("9sellinventory").child("shoes").child("b22mhFiqHuVR9vwowQyQjD4720Q2").getChildren()) {
                     //for() {
                     shoearraypass.add("" + snaparray.child("shoeImageUrl").getValue());
                     classshoe.add(new Shoe(snaparray));
