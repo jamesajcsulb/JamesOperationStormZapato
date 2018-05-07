c636b86fb749c0a4d903f1786c25af61af1bcb13
Agustin Barajas <agustincards14@gmail.com>
2018-05-07 05:15:05 -0700

    Added upload photo feature & updated app icon

--- app/src/main/java/com/example/james/myapplication/add/ShareFragment.java
+++ app/src/main/java/com/example/james/myapplication/add/ShareFragment.java
@@ -4,7 +4,9 @@ package com.example.james.myapplication.add;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -21,6 +23,9 @@ import android.widget.Toast;
 
 import com.example.james.myapplication.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class ShareFragment extends Fragment {
@@ -41,6 +46,7 @@ public class ShareFragment extends Fragment {
     private ImageView photo3;
     private ImageView photo4;
     Button takePicButton;
+    Button uploadPicButton;
     int pictureCount = 0;
 
     @Override
@@ -59,6 +65,7 @@ public class ShareFragment extends Fragment {
         //ButterKnife.bind(this, view);
 
         editTitle = view.findViewById(R.id.textInputLayout2);
+        uploadPicButton = view. findViewById(R.id.uploadphoto_button);
         takePicButton = view.findViewById(R.id.takephoto_button);
         photo1= view.findViewById(R.id.imageView);
         photo2= view.findViewById(R.id.imageView2);
@@ -108,23 +115,50 @@ public class ShareFragment extends Fragment {
             }
         });
 
-
         takePicButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dispatchTakePictureIntent();
             }
         });
+
+        uploadPicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchUploadPicIntent();
+            }
+        });
+    }
+
+    // TODO: Crop picture from gallery
+    public static final int GET_FROM_GALLERY = 3;
+    /**
+     * this function runs when User clicks "Upload picture" button
+     */
+    private void dispatchUploadPicIntent() {
+        pictureCount++;
+        if (pictureCount>4) {
+            getView().findViewById(R.id.takephoto_button).setClickable(false);
+            Toast.makeText(getContext(), "Maximum picture upload reached!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File cameraPhotoFile = new File(path, System.currentTimeMillis()+".jpg"); //timestamp makes this file name unique for immediate retrieval
+        startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+
     }
 
 
-
     static final int REQUEST_IMAGE_CAPTURE = 1;
-
+    /**
+     * this function runs when User clicks "Take picture" button
+     */
     private void dispatchTakePictureIntent() {
         pictureCount++;
         if (pictureCount>4) {
             getView().findViewById(R.id.takephoto_button).setClickable(false);
+            Toast.makeText(getContext(), "Maximum picture upload reached!", Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -134,25 +168,94 @@ public class ShareFragment extends Fragment {
         }
     }
 
+    /**
+     * this function runs when Camera access is granted
+     */
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("onActivityResult", "requestCode: "+requestCode);
+
+        if (resultCode!=Activity.RESULT_OK) return;
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            Log.d("REQUEST_IMAGE_CAPTURE", "requestCode: "+requestCode);
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            switch (pictureCount) {
-                case 1: photo1.setImageBitmap(imageBitmap); photo1.setRotation(90);
-                    break;
-                case 2: photo2.setImageBitmap(imageBitmap); photo2.setRotation(90);
-                    break;
-                case 3: photo3.setImageBitmap(imageBitmap); photo3.setRotation(90);
-                    break;
-                case 4: photo4.setImageBitmap(imageBitmap); photo4.setRotation(90);
-                    break;
-                default:
-                    break;
+            setThumbnails(REQUEST_IMAGE_CAPTURE, imageBitmap);
+        }
+        else if (requestCode == GET_FROM_GALLERY) {
+            Log.d("GET_FROM_GALLERY", "requestCode: "+requestCode);
+            Uri selectedImage = data.getData();
+            Bitmap imageBitmap = null;
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            setThumbnails(GET_FROM_GALLERY, imageBitmap);
         }
     }
+
+    public void setThumbnails(int code, Bitmap imageBitmap) {
+        boolean rotate=true;
+        if (code==GET_FROM_GALLERY) {rotate=false;}
+        else if (code==REQUEST_IMAGE_CAPTURE) {rotate=true;}
+
+        switch (pictureCount) {
+            case 1: photo1.setImageBitmap(imageBitmap);
+                    if (rotate) photo1.setRotation(90);
+                break;
+            case 2: photo2.setImageBitmap(imageBitmap);
+                    if (rotate) photo2.setRotation(90);
+                break;
+            case 3: photo3.setImageBitmap(imageBitmap);
+                    if (rotate) photo3.setRotation(90);
+                break;
+            case 4: photo4.setImageBitmap(imageBitmap);
+                    if (rotate) photo4.setRotation(90);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    /**
+     * this function does the crop operation.
+     */
+//    private void performCrop() {
+////        Log.d("performCrop", "picUriPATH: "+picUri.getPath());
+//        // take care of exceptions
+//        try {
+//            // call the standard crop action intent (the user device may not
+//            // support it)
+//            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+//            // indicate image type and Uri
+//            cropIntent.setDataAndType(picUri, "image/*");
+//            // set crop properties
+//            cropIntent.putExtra("crop", "true");
+//            // indicate aspect of desired crop
+//            cropIntent.putExtra("aspectX", 2);
+//            cropIntent.putExtra("aspectY", 1);
+//            // indicate output X and Y
+//            cropIntent.putExtra("outputX", 256);
+//            cropIntent.putExtra("outputY", 256);
+//            // retrieve data on return
+//            cropIntent.putExtra("return-data", true);
+//            // start the activity - we handle returning in onActivityResult
+//            startActivityForResult(cropIntent, CROP_PIC);
+//        }
+//        // respond to users whose devices do not support the crop action
+//        catch (ActivityNotFoundException e) {
+//            Toast toast = Toast.makeText(getContext(), "This device doesn't support the crop action!", Toast.LENGTH_SHORT);
+//            toast.show();
+//        }
+//    }
+
 
 
 }
